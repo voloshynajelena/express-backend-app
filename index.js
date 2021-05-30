6528063c72dfa0e8e812dc01255a85887f02bd18
Voloshyna Jelena <codeismagicschool@gmail.com>
2021-05-30 17:19:11 +0300

    mongoDb connection, adding course to database

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const path = require('path')
+const mongoose = require('mongoose')
 const exphbs = require('express-handlebars')
 const homeRoutes = require('./routes/home')
 const cardRoutes = require('./routes/card')
@@ -27,6 +28,18 @@ app.use('/card', cardRoutes)
 
 const PORT = process.env.PORT || 3000
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`)
-})
+async function start() {
+  try {
+    const url = `mongodb+srv://elena:0I5GEL9uLUcR38GC@cluster0.evtea.mongodb.net/shop`
+    await mongoose.connect(url, {useNewUrlParser: true})
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`)
+    })
+  } catch (e) {
+    console.log(e)
+  }
+}
+
+start()
+
+
---
+++ pass.js
@@ -0,0 +1,3 @@
+
+const url = 'mongodb+srv://elena:0I5GEL9uLUcR38GC@cluster0.evtea.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+const url2 = 'mongodb+srv://elena:0I5GEL9uLUcR38GC@cluster0.evtea.mongodb.net/shop';
