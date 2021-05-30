f192be1138a7ebae971c35d0b8f55ebde8264112
Voloshyna Jelena <codeismagicschool@gmail.com>
2021-05-30 18:47:33 +0300

    mongoDb: orders

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const exphbs = require('express-handlebars')
 const homeRoutes = require('./routes/home')
 const cardRoutes = require('./routes/card')
 const addRoutes = require('./routes/add')
+const ordersRoutes = require('./routes/orders')
 const coursesRoutes = require('./routes/courses')
 const User = require('./models/user')
 
@@ -36,6 +37,7 @@ app.use('/', homeRoutes)
 app.use('/add', addRoutes)
 app.use('/courses', coursesRoutes)
 app.use('/card', cardRoutes)
+app.use('/orders', ordersRoutes)
 
 const PORT = process.env.PORT || 3000
 
