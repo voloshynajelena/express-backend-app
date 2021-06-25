64f26056a097654a486366efad71225b3a105516
Voloshyna Jelena <codeismagicschool@gmail.com>
2021-06-25 21:30:01 +0300

    auth: finalization

--- pass.js
+++ pass.js
@@ -1,9 +1,3 @@
 
 const url = 'mongodb+srv://elena:0I5GEL9uLUcR38GC@cluster0.evtea.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
 const url2 = 'mongodb+srv://elena:0I5GEL9uLUcR38GC@cluster0.evtea.mongodb.net/shop';
-const userId = '60b3a76c2a5aa1d18885a22e';
-const user = {
-  email: 'voloshynajelena@gmail.com',
-  name: 'Elena',
-  cart: {items: []}
-}
