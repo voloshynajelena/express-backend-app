9c6740ba320204b6d165c6269d1b5654524bad75
Voloshyna Jelena <codeismagicschool@gmail.com>
2021-06-25 20:39:21 +0300

    auth: storing session in mongoDB

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const path = require('path')
 const mongoose = require('mongoose')
 const exphbs = require('express-handlebars')
 const session = require('express-session')
+const MongoStore = require('connect-mongodb-session')(session)
 const homeRoutes = require('./routes/home')
 const cardRoutes = require('./routes/card')
 const addRoutes = require('./routes/add')
@@ -12,11 +13,15 @@ const authRoutes = require('./routes/auth')
 const User = require('./models/user')
 const varMiddleware = require('./middleware/variables')
 
+const MONGODB_URI = `mongodb+srv://elena:0I5GEL9uLUcR38GC@cluster0.evtea.mongodb.net/shop`
 const app = express()
-
 const hbs = exphbs.create({
   defaultLayout: 'main',
   extname: 'hbs'
+})
+const store = new MongoStore({
+  collection: 'sessions',
+  uri: MONGODB_URI
 })
 
 app.engine('hbs', hbs.engine)
@@ -28,7 +33,8 @@ app.use(express.urlencoded({extended: true}))
 app.use(session({
   secret: 'some secret value',
   resave: false,
-  saveUninitialized: false
+  saveUninitialized: false,
+  store
 }))
 app.use(varMiddleware)
 
@@ -43,8 +49,7 @@ const PORT = process.env.PORT || 3000
 
 async function start() {
   try {
-    const url = `mongodb+srv://elena:0I5GEL9uLUcR38GC@cluster0.evtea.mongodb.net/shop`
-    await mongoose.connect(url, {
+    await mongoose.connect(MONGODB_URI, {
       useNewUrlParser: true,
       useFindAndModify: false
     })
