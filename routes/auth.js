5d92f5f13dd2b909de4b543537ff070cabf7446b
Voloshyna Jelena <codeismagicschool@gmail.com>
2021-06-25 21:09:29 +0300

    auth: user login

--- routes/auth.js
+++ routes/auth.js
@@ -16,15 +16,31 @@ router.get('/logout', async (req, res) => {
 })
 
 router.post('/login', async (req, res) => {
-  const user = await User.findById('60b3a76c2a5aa1d18885a22e')
-  req.session.user = user
-  req.session.isAuthenticated = true
-  req.session.save(err => {
-    if (err) {
-      throw err
+  try {
+    const {email, password} = req.body
+    const candidate = await User.findOne({ email })
+
+    if (candidate) {
+      const areSame = password === candidate.password
+
+      if (areSame) {
+        req.session.user = candidate
+        req.session.isAuthenticated = true
+        req.session.save(err => {
+          if (err) {
+            throw err
+          }
+          res.redirect('/')
+        })
+      } else {
+        res.redirect('/auth/login#login')
+      }
+    } else {
+      res.redirect('/auth/login#login')
     }
-    res.redirect('/')
-  })
+  } catch (e) {
+    console.log(e)
+  }
 })
 
 router.post('/register', async (req, res) => {
