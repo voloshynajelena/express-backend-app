e5d465f28385f9dcfae4532380589c3131a9c971
Voloshyna Jelena <codeismagicschool@gmail.com>
2021-06-25 20:31:53 +0300

    auth: saving session

--- index.js
+++ index.js
@@ -23,16 +23,6 @@ app.engine('hbs', hbs.engine)
 app.set('view engine', 'hbs')
 app.set('views', 'views')
 
-app.use(async (req, res, next) => {
-  try {
-    const user = await User.findById('60b3a76c2a5aa1d18885a22e')
-    req.user = user
-    next()
-  } catch (e) {
-    console.log(e)
-  }
-})
-
 app.use(express.static(path.join(__dirname, 'public')))
 app.use(express.urlencoded({extended: true}))
 app.use(session({
@@ -58,15 +48,6 @@ async function start() {
       useNewUrlParser: true,
       useFindAndModify: false
     })
-    const candidate = await User.findOne()
-    if (!candidate) {
-      const user = new User({
-        email: 'voloshynajelena@gmail.com',
-        name: 'Elena',
-        cart: {items: []}
-      })
-      await user.save()
-    }
     app.listen(PORT, () => {
       console.log(`Server is running on port ${PORT}`)
     })
--- routes/auth.js
+++ routes/auth.js
@@ -1,4 +1,5 @@
 const {Router} = require('express')
+const User = require('../models/user')
 const router = Router()
 
 router.get('/login', async (req, res) => {
@@ -15,8 +16,15 @@ router.get('/logout', async (req, res) => {
 })
 
 router.post('/login', async (req, res) => {
+  const user = await User.findById('60b3a76c2a5aa1d18885a22e')
+  req.session.user = user
   req.session.isAuthenticated = true
-  res.redirect('/')
+  req.session.save(err => {
+    if (err) {
+      throw err
+    }
+    res.redirect('/')
+  })
 })
 
 module.exports = router
