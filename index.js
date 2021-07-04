a14554a1e3fb77bdb0dd6afded645c6ba1d7aa1d
Voloshyna Jelena <codeismagicschool@gmail.com>
2021-07-04 23:04:18 +0300

    page for 404

--- index.js
+++ index.js
@@ -14,6 +14,7 @@ const coursesRoutes = require('./routes/courses')
 const authRoutes = require('./routes/auth')
 const varMiddleware = require('./middleware/variables')
 const userMiddleware = require('./middleware/user')
+const errorHandler = require('./middleware/error')
 const keys = require('./keys')
 
 const PORT = process.env.PORT || 3000
@@ -53,6 +54,8 @@ app.use('/courses', coursesRoutes)
 app.use('/card', cardRoutes)
 app.use('/orders', ordersRoutes)
 app.use('/auth', authRoutes)
+
+app.use(errorHandler)
 
 async function start() {
   try {
