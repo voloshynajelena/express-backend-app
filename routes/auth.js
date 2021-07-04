45de05c338dc87042b4357a2b607d5e3c5295d91
Voloshyna Jelena <codeismagicschool@gmail.com>
2021-07-04 22:29:42 +0300

    validation of registration form

--- routes/auth.js
+++ routes/auth.js
@@ -1,12 +1,14 @@
 const {Router} = require('express')
 const bcrypt = require('bcryptjs')
 const crypto = require('crypto')
+const {validationResult} = require('express-validator/check')
 const nodemailer = require('nodemailer')
 const sendgrid = require('nodemailer-sendgrid-transport')
 const User = require('../models/user')
 const keys = require('../keys')
 const regEmail = require('../emails/registration')
 const resetEmail = require('../emails/reset')
+const {registerValidators} = require('../utils/validators')
 const router = Router()
 
 const transporter = nodemailer.createTransport(sendgrid({
@@ -58,10 +60,16 @@ router.post('/login', async (req, res) => {
   }
 })
 
-router.post('/register', async (req, res) => {
+router.post('/register', registerValidators, async (req, res) => {
   try {
-    const {email, password, repeat, name} = req.body
+    const {email, password, confirm, name} = req.body
     const candidate = await User.findOne({ email })
+
+    const errors = validationResult(req)
+    if (!errors.isEmpty()) {
+      req.flash('registerError', errors.array()[0].msg)
+      return res.status(422).redirect('/auth/login#register')
+    }
 
     if (candidate) {
       req.flash('registerError', 'Пользователь с таким email уже существует')
