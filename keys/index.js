60371288a356794ebd008b730f474b80ffbe8d6f
Voloshyna Jelena <codeismagicschool@gmail.com>
2021-07-05 00:10:57 +0300

    downloading files finalize

--- keys/index.js
+++ keys/index.js
@@ -1,7 +1,7 @@
 module.exports = {
-  MONGODB_URI: 'mongodb+srv://vladilen:0I5GEL9uLUcR38GC@cluster0-3rrau.mongodb.net/shop',
+  MONGODB_URI: 'mongodb+srv://elena:0I5GEL9uLUcR38GC@cluster0.evtea.mongodb.net/shop',
   SESSION_SECRET: 'some secret value',
-  SENDGRID_API_KEY: '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',
+  SENDGRID_API_KEY: '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',
   EMAIL_FROM: 'nodejs@email.ru',
   BASE_URL: 'http://localhost:3000'
 }
