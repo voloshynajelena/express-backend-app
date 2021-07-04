f65b2c2ded4110af569684428b546b9e3ef749d4
Voloshyna Jelena <codeismagicschool@gmail.com>
2021-07-05 00:53:11 +0300

    gitignore settings

--- keys/keys.dev.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  MONGODB_URI: 'mongodb+srv://elena:0I5GEL9uLUcR38GC@cluster0.evtea.mongodb.net/shop',
-  SESSION_SECRET: 'some secret value',
-  SENDGRID_API_KEY: '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',
-  EMAIL_FROM: 'nodejs@email.ru',
-  BASE_URL: 'http://localhost:3000'
-}
