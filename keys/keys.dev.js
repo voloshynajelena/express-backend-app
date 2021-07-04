dfb32b513c32a41b1e2fe3248fecb05ed5c3eb77
Voloshyna Jelena <codeismagicschool@gmail.com>
2021-07-05 01:00:51 +0300

    gitignore settings

---
+++ keys/keys.dev.js
@@ -0,0 +1,7 @@
+module.exports = {
+  MONGODB_URI: 'mongodb+srv://elena:0I5GEL9uLUcR38GC@cluster0.evtea.mongodb.net/shop',
+  SESSION_SECRET: 'some secret value',
+  SENDGRID_API_KEY: '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',
+  EMAIL_FROM: 'nodejs@email.ru',
+  BASE_URL: 'http://localhost:3000'
+}
--- pass.js
+++
@@ -1,4 +0,0 @@
-
-const url = 'mongodb+srv://elena:0I5GEL9uLUcR38GC@cluster0.evtea.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
-const url2 = 'mongodb+srv://elena:0I5GEL9uLUcR38GC@cluster0.evtea.mongodb.net/shop';
-const sendGrid = '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';
